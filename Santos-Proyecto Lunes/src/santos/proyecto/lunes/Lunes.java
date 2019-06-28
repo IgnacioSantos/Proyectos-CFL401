03ce38a331d0bf60840351d91270e9a34f312081
Programación <Programacin@DESKTOP-6D4UVF0>
2019-06-28 20:05:58 -0300

    Se modifico y se agrego una nueva ventana

--- Santos-Proyecto Lunes/src/santos/proyecto/lunes/Lunes.java
+++ Santos-Proyecto Lunes/src/santos/proyecto/lunes/Lunes.java
@@ -10,15 +10,22 @@ package santos.proyecto.lunes;
  * @author Programación
  */
 public class Lunes {
-
+public static VentanaLogin ventanaLogin = null;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ventanaLogin = new VentanaLogin();
+        iniciar();
+        
+        
         System.out.println("Hola mundo");
         
-        
+        System.out.println("Adios");
     }
     
+    public static void iniciar(){
+        ventanaLogin.setVisible(true);
+    }
 }
