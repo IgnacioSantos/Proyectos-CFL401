d6cea56096eb8f9a4da561a4c18aa755c15aacb6
Programación <Programacin@DESKTOP-6D4UVF0>
2019-06-28 19:34:42 -0300

    Se modifico e agregando un mostrar mensaje

--- Santos-Proyecto Lunes/src/santos/proyecto/lunes/Lunes.java
+++ Santos-Proyecto Lunes/src/santos/proyecto/lunes/Lunes.java
@@ -1,5 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+4 * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -16,6 +16,9 @@ public class Lunes {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
+        
+        
     }
     
 }
