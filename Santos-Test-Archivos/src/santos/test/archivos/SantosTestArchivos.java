d8288793f2ae87dc66a376d4d017a617fb949aad
Programación <Programacin@DESKTOP-6D4UVF0>
2019-07-12 20:34:14 -0300

    Test-Archivos: Reforzamos el proyecto

--- Santos-Test-Archivos/src/santos/test/archivos/SantosTestArchivos.java
+++ Santos-Test-Archivos/src/santos/test/archivos/SantosTestArchivos.java
@@ -27,16 +27,16 @@ public class SantosTestArchivos {
         agregarArchivoTexto();
         leerArchivoTexto();
         VentanaLogin ventana = new VentanaLogin();//creamos un objeto del tipo de dato (VentanaLogin)
-        
+
         ventana.setVisible(true);//hacemos que se muestre al usuario ya que debe tener un valor true para eso
-        
+        //se tiene que agregar /** y terminar con*/ para hacer la documentacion de la funcion de cada clase
     }
-    
-    private static void agregarArchivoTexto(){
+
+    private static void agregarArchivoTexto() {
         FileWriter file = null;//la clase que va a conectarse con el archivo dentro del parentesis puede haber (ruta al archivo,sobreescribir)-("texto.txt",false)
         PrintWriter escritor = null;
         try {
-            file = new FileWriter("texto.txt",false);//tener las conexiones true sobreescribe el archivo si es false lo borra el contenido y empieza de nuevo
+            file = new FileWriter("texto.txt", false);//tener las conexiones true sobreescribe el archivo si es false lo borra el contenido y empieza de nuevo
             escritor = new PrintWriter(file);//se tiene que pasar por parentesis el enlace
             escritor.println("Hola");//mandamos una escritura en string
             //escritor.print("edad:");
@@ -46,24 +46,24 @@ public class SantosTestArchivos {
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-         
+
     }
-    
-    private static void leerArchivoTexto(){
+
+    private static void leerArchivoTexto() {
         FileReader file = null;//genera un archivo
         BufferedReader lector = null;//leera el archivo
-        
-        try{
+
+        try {
             file = new FileReader("users.txt");//encuentra un archivo y lo tratara de leerlo e interpretarlo
             lector = new BufferedReader(file);//generara una memoria llamada Buffer y lo graba en el PC
             String texto = lector.readLine();//le asignamos una variable e inicialezamos para su lectura
-            
-            while(texto!=null){//cuando es distinto a null seguira mostrando el contenido que tiene el texto
+
+            while (texto != null) {//cuando es distinto a null seguira mostrando el contenido que tiene el texto
                 //System.out.println(texto);
                 System.out.println(texto.split(";;;")[0]);//separa los contenidos en base a su especificacion dentro de los (" ")
-                System.out.println(texto.split(";;;")[1]+"\n");//tambien se transforma en un array de string
+                System.out.println(texto.split(";;;")[1] + "\n");//tambien se transforma en un array de string
                 texto = lector.readLine();
-              /*
+                /*
                 try{
                 String[] tp;
                 String texto = lector.readLine();
@@ -78,10 +78,10 @@ public class SantosTestArchivos {
                 }catch(IOExeption ex){
                 
                 }
-                */
+                 */
             }
             lector.close();
-        }catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
