7371738a0eda07df042005d1c7ad82bf68ed1f91
Programación <Programacin@DESKTOP-6D4UVF0>
2019-07-15 19:43:37 -0300

    Test-Archivos: Agregamos una nueva clase

---
+++ Santos-Test-Archivos/src/santos/test/archivos/Testeo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package santos.test.archivos;
+
+/**
+ *
+ * @author Programación
+ */
+public class Testeo {
+
+    private static String apellido;
+    private int edad;
+
+    public static void setApellido(String a) {
+        apellido = a;
+    }
+
+    public static String getApellido() {
+        return apellido;
+    }
+
+}
