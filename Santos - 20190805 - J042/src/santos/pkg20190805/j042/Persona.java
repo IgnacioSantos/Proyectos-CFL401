bc7ba58e666cbaf7b0104a3e6acfb8c90258d93f
Programación <Programacin@DESKTOP-6D4UVF0>
2019-08-05 21:30:23 -0300

    Java mascota: creamos una actividad

---
+++ Santos - 20190805 - J042/src/santos/pkg20190805/j042/Persona.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package santos.pkg20190805.j042;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Programación
+ */
+public class Persona {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int dni;
+    private Mascota[] mascotas;
+    private ArrayList <Mascota> mascotas;
+
+    public Mascota[] getMascota() {
+        return mascotas;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setMascota(Mascota[] mascota) {
+        this.mascotas = mascota;
+    }
+
+    public Persona(int id, String nombre, String apellido, int edad, int dni) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dni = dni;
+        mascotas = new ArrayList();
+    }
+    
+    public boolean estaLaMascota(String nombre){
+        return  
+    }
+    
+    public int cuantasMascotasTenes(){
+        return mascotas.length;
+                
+    }
+    
+    public boolean sosMayorA(int edad){
+        return this.edad<edad;
+    }
+
+    
+}
+
+
