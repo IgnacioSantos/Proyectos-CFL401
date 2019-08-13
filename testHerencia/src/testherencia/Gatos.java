fe3139b90c62c0113b03cdba3c2ada096cee2ebe
Programación <Programacin@DESKTOP-6D4UVF0>
2019-08-12 21:20:27 -0300

    testHerencia: creamos una nueva clase

---
+++ testHerencia/src/testherencia/Gatos.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testherencia;
+
+/**
+ *
+ * @author Programación
+ */
+public class Gatos extends Animales {
+
+    private int cantPelos;
+
+    public Gatos(String nombre, int patas, int cantPelos) {
+        super(nombre,patas,100);// se utiliza para la re-utilizacion del contructor PADRE
+        this.cantPelos = cantPelos;
+        
+        
+        /*this.patas = patas;
+        this.cantPelos = cantPelos;
+        energia = 100;*/
+    }
+
+    public int getCantPelos() {
+        return cantPelos;
+    }
+
+    public void setCantPelos(int cantPelos) {
+        this.cantPelos = cantPelos;
+    }
+
+    public void correr() {
+        energia = energia - 5;
+    }
+
+    public void jugar() {
+        energia = energia - 10;
+    }
+
+    @Override
+    public void comer(String nomComida) {
+        if (nomComida.equals("Raza")) {
+            energia = energia + 15;
+        }
+
+    }
+
+    public void jugarCon(Gatos gato) {
+        if (energia > 20) {
+            jugar();
+            gato.jugar();
+        }
+    }
+}
