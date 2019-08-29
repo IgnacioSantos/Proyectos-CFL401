b2617df6663d0cce3f33eeadc29306a168280565
Programación <Programacin@DESKTOP-6D4UVF0>
2019-08-28 21:21:24 -0300

    Base de Datos: AGREGO COMENTARIOS

--- Santos-20190826-BaseDeDatos/src/santos/pkg20190826/basededatos/Santos20190826BaseDeDatos.java
+++ Santos-20190826-BaseDeDatos/src/santos/pkg20190826/basededatos/Santos20190826BaseDeDatos.java
@@ -17,11 +17,11 @@ public class Santos20190826BaseDeDatos {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException {
-        ResultSet resultado;
+        ResultSet resultado;//creamos una variable con un tipo de dato resultset 
         Connection conexion;
-        conexion = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/cfl401?user=programador&password=cfl401");
+        conexion = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/cfl401?user=programador&password=cfl401");//sirve para conectarse a la ruta donde se aloja la base de datos con su respectiva direcion y puerto con la tabla y usando ? es como espacio y se agrega el usuario y la contraseña.
         Statement consultor = conexion.createStatement();
-        resultado = consultor.executeQuery("SELECT * FROM accesos");
+        resultado = consultor.executeQuery("SELECT * FROM accesos");//usando el execute.query() consultamos en formato string igual los comandos introducidos en el mariadb.
         while (resultado.next()) {
             System.out.println(resultado.getInt("id"));
             System.out.println(resultado.getString("usuario"));
